ae41145cc8fdd05ca1960cac8d89f0b09d593e10
Andrew Koroti <korontos@mail.ru>
2024-03-01 10:57:45 +0300

    '#'

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -1,3 +1,28 @@
-print ("enter number")
-a = input()
-print(a)
+# 1 Разные способы выводов на печать
+# a = 5
+# b = 5.54
+# c = "sad"
+#
+# print(a,'-',b,'-',c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}" .format(a,b,c))
+
+# 2 Разный ввод и печать как str или int
+# print('Enter first number')
+# a = input()
+#
+# b = input('Enter second number: ')
+#
+# print(a, "+", b, '=', a + b)
+# a = int(a)
+# b = int(b)
+# print(a, "+", b, '=', a + b)
+#
+# c = int(input('Enter second number: '))
+# d = int(input('Enter second number: '))
+# print(c, "+", d, '=', c + d)
+
+# Округление
+# a = 5.8955
+# b = 8.556
+# print(round(a * b, 3)) #  3 Кол-во знаков после запятой
